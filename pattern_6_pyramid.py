d36a3890a77146e255946c294485306e284f255d
BoyanKostadinov <boyan.sv.kostadinov@gmail.com>
2024-05-25 10:10:09 +0300

    changed counter in pyramid pattern

--- pattern_6_pyramid.py
+++ pattern_6_pyramid.py
@@ -2,7 +2,7 @@
 
 number_of_rows = int(input())
 
-upper_half_counter = number_of_rows - 1
+counter_blank_spaces = number_of_rows - 1
 for row in range(1, 2 * number_of_rows, 2):
-    print(upper_half_counter * ' ' + '*' * row)
-    upper_half_counter -= 1
+    print(counter_blank_spaces * ' ' + '*' * row)
+    counter_blank_spaces -= 1
